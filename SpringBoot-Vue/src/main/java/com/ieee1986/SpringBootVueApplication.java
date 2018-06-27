cc441028e9644e2103f896f46d2b726b28ba4c58
jeonjehun <jeonjehun86@gmail.com>
2018-06-27 13:12:25 +0900

    1.SpringBoot 프로젝트 생성

---
+++ SpringBoot-Vue/src/main/java/com/ieee1986/SpringBootVueApplication.java
@@ -0,0 +1,12 @@
+package com.ieee1986;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVueApplication.class, args);
+	}
+}
